cf5b15f5508a3ef59240927b0f62b7b7e3cb9417
HasanAlii73 <ha1338967@gmail.com>
2025-06-06 03:13:52 +0300

    Classify Project

--- script.js
+++ script.js
@@ -1,41 +1,4 @@
-let myLibrary = [  
-    {
-        title: "48 laws of power",
-        author: "Robert Greene",
-        pages: 635,
-        read: true,
-        genre: "Self-help-book"
-    },
-    {
-        title: "Surrounded by idiots",
-        author: "Thomas Erikson",
-        pages: 320,
-        read: true,
-        genre: "Self-help-book"
-    },
-    {
-        title: "The Atomic Habits",
-        author: "James Clear",
-        pages: 291,
-        read: true,
-        genre: "Self-help-book"
-    },
-    {
-        title: "The Prince",
-        author: "Niccolo Machiavelli",
-        pages: 126,
-        read: true,
-        genre: "Non-fiction"
-    },
-    {
-        title: "Ego is the enemy",
-        author: "Ryan Holiday",
-        pages: 287,
-        read: true,
-        genre: "Self-help-book"
-    }];
 const library = document.querySelector(".library");
-
 const dialog = document.querySelector("dialog");
 const addNewBtn = document.querySelector(".btnAddNewBook");
 const closeBtn = document.querySelector("dialog button");
@@ -43,61 +6,107 @@ const addBtn = document.querySelector(".btnAdd");
 const form = document.querySelector("#book-form");
 const overlay = document.querySelector(".dialog-overlay");
 
-renderLibrary();
 
-function Book(id, title, author, pages, genre, read) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.genre = genre;
-    this.read = read;
+class Book{
+    constructor(id, title, author, pages, genre, read){
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.genre = genre;
+        this.read = read;
+    }
 }
 
-function addBookToLibrary(title, author, pages, genre, read) {
-    const id = crypto.randomUUID();
-    const book = new Book(id, title, author, pages, genre, read);
-    myLibrary.push(book);
-    renderLibrary();
+class Library{
+
+    constructor(myLibrary){
+        this.myLibrary = myLibrary;
+    }
+
+    addBookToLibrary(title, author, pages, genre, read) {
+        const id = crypto.randomUUID();
+        const book = new Book(id, title, author, pages, genre, read);
+        this.myLibrary.push(book);
+        this.renderLibrary();
+    }
+
+    renderLibrary() {
+        library.innerHTML = '';
+    
+        this.myLibrary.forEach((book, index) => {
+            const bookCard = document.createElement('div');
+            bookCard.className = 'book-card';
+    
+            bookCard.innerHTML = `
+                    <h3>${book.title}</h3>
+                    <p>by ${book.author}</p>
+                    <p>${book.pages} pages</p>
+                    <p>Genre: ${book.genre}</p>
+                    <div class="read-status ${book.read ? 'read' : 'unread'}">
+                        ${book.read ? 'Read' : 'Unread'}
+                    </div>
+                    <button class="toggle-read-btn" data-index="${index}">
+                        Toggle Read Status
+                    </button>
+                    <button class="remove-btn" data-index="${index}">
+                        Remove
+                    </button>
+                `;
+    
+            library.appendChild(bookCard);
+        });
+    }
+    
+    toggleReadStatus(index) {
+        this.myLibrary[index].read = !this.myLibrary[index].read;
+        this.renderLibrary();
+    }
+    
+    removeBook(index) {
+        this.myLibrary.splice(index, 1);
+        this.renderLibrary();
+    }
 }
 
-function renderLibrary() {
-    library.innerHTML = '';
-
-    myLibrary.forEach((book, index) => {
-        const bookCard = document.createElement('div');
-        bookCard.className = 'book-card';
-
-        bookCard.innerHTML = `
-                <h3>${book.title}</h3>
-                <p>by ${book.author}</p>
-                <p>${book.pages} pages</p>
-                <p>Genre: ${book.genre}</p>
-                <div class="read-status ${book.read ? 'read' : 'unread'}">
-                    ${book.read ? 'Read' : 'Unread'}
-                </div>
-                <button class="toggle-read-btn" data-index="${index}">
-                    Toggle Read Status
-                </button>
-                <button class="remove-btn" data-index="${index}">
-                    Remove
-                </button>
-            `;
-
-        library.appendChild(bookCard);
-    });
-}
-
-function toggleReadStatus(index) {
-    myLibrary[index].read = !myLibrary[index].read;
-    renderLibrary();
-}
-
-function removeBook(index) {
-    myLibrary.splice(index, 1);
-    renderLibrary();
-}
-
+let myLib = [  
+        {
+            title: "48 laws of power",
+            author: "Robert Greene",
+            pages: 635,
+            read: true,
+            genre: "Self-help-book"
+        },
+        {
+            title: "Surrounded by idiots",
+            author: "Thomas Erikson",
+            pages: 320,
+            read: true,
+            genre: "Self-help-book"
+        },
+        {
+            title: "The Atomic Habits",
+            author: "James Clear",
+            pages: 291,
+            read: true,
+            genre: "Self-help-book"
+        },
+        {
+            title: "The Prince",
+            author: "Niccolo Machiavelli",
+            pages: 126,
+            read: true,
+            genre: "Non-fiction"
+        },
+        {
+            title: "Ego is the enemy",
+            author: "Ryan Holiday",
+            pages: 287,
+            read: true,
+            genre: "Self-help-book"
+        }];
+let lib = new Library(myLib);
+lib.renderLibrary();
 
 addNewBtn.addEventListener("click", () => {
     dialog.showModal();
@@ -117,21 +126,22 @@ form.addEventListener("submit", (event) => {
     const pages = document.getElementById("inPages").value;
     const genre = document.getElementById("inGenre").value;
     const isRead = document.getElementById("inRead").checked;
-    addBookToLibrary(title, author, pages, genre, isRead);
+    lib.addBookToLibrary(title, author, pages, genre, isRead);
     form.reset();
     dialog.close();
+    overlay.style.display = "none";
 });
 
 document.addEventListener("click", function(e) {
     if (e.target.classList.contains("toggle-read-btn")) {
         const index = e.target.dataset.index;
-        toggleReadStatus(index);
+        lib.toggleReadStatus(index);
     }
 });
 
 document.addEventListener("click", function(e) {
     if (e.target.classList.contains("remove-btn")) {
         const index = e.target.dataset.index;
-        removeBook(index);
+        lib.removeBook(index);
     }
 });
